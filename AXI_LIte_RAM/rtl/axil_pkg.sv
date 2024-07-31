d196782123302f2be36fe0ffb8fe7de3ed13283f
SemenovMD <makcsem64@gmail.com>
2024-07-31 14:05:41 -0600

    Added AXI_Lite_RAM

---
+++ AXI_LIte_RAM/rtl/axil_pkg.sv
@@ -0,0 +1,8 @@
+package axil_pkg;
+
+    parameter                               AXI_DATA_WIDTH                  = 32;
+    parameter                               AXI_ADDR_WIDTH                  = 32;
+
+    parameter                               MEMORY_RAM                      = 1024;
+
+endpackage
